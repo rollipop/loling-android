7a7916cb6bc6bdaec9d4e82ac4c1e30104229030
dbwjd <rladbwjd341@naver.com>
2019-01-21 04:56:34 +0900

    로그인과 관련된 activity와 layout xml을 추가한다
    - LoginActivity, SignUpPersonalActivity, SignupEntryActivity, SignupPhotoActivity 생성 및 레이아웃 파일 생성.
    - base color 추가
    - login.xml 형성
    //현재 제플린에 올라온 디자인 규격이 맞지 않아 수치기입을 할 수 없어 하드코딩하는 점 양해 부탁드립니다..
    
    Fixes https://github.com/chaeniiz/calculator/issues/2

---
+++ app/src/main/java/mashup/loling/SignupEntryActivity.kt
@@ -0,0 +1,12 @@
+package mashup.loling
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SignupEntryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_entry)
+    }
+}
