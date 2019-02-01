f566b83a74faf149a3d9ea7d34c7b34add09e31a
김유정 <pointu341@gmail.com>
2019-02-02 02:02:32 +0900

    에러 해결

--- app/src/main/java/mashup/loling/MainActivity.kt
+++ app/src/main/java/mashup/loling/MainActivity.kt
@@ -2,6 +2,7 @@ package mashup.loling
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import mashup.loling.model.FriendItem
 
 class MainActivity : AppCompatActivity() {
 
