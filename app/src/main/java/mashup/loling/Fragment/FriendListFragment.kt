e121df83620d1df1f6f287b3e4443b6d9f5132fe
yoo-chaewon <ohwow5@sookmyung.ac.kr>
2019-02-01 23:19:25 +0900

    Add MyPageActivity
    MyPageActivity 만들기
    
    FriendListFragment, FriendAdapter, FriendItem
    RecivedListFragment, LolingItemAdapter
    WrittenListFragment
    MyPageSettingActivity 아직

---
+++ app/src/main/java/mashup/loling/Fragment/FriendListFragment.kt
@@ -0,0 +1,38 @@
+package mashup.loling.Fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import mashup.loling.Adapter.FriendItemAdapter
+import mashup.loling.model.FriendItem
+import mashup.loling.R
+
+class FriendListFragment : Fragment {
+    constructor() : super()
+    val friends: ArrayList<FriendItem> = ArrayList()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val root = inflater.inflate(R.layout.recyclerview, container, false)
+
+        val recyclerView = root.findViewById<RecyclerView>(R.id.recyclerView)
+        if(recyclerView != null) {
+            recyclerView.layoutManager = LinearLayoutManager(context)//this.context/getContext()
+            makeFriendList()
+            recyclerView.adapter = FriendItemAdapter(friends)
+        }
+
+        return root
+
+    }
+
+    fun makeFriendList(){//친구목록 가져와 처리
+        for (i in 0..9){
+            friends.add(FriendItem("유채원" + i, "D-10" + i, "1996.03.22"))
+        }
+    }
+
+}
