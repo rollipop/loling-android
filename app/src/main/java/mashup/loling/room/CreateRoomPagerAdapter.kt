3b9f701ac1a36b00836b9a912985ce79496f507f
Jujin <jujin@jujinkim.com>
2019-02-12 02:51:57 +0900

    방 생성 팝업 액티비티에서, 각 페이지를 프래그먼트 안쓰고 걍 레이아웃으로 처리하도록 바꿈
    이런거에 프래그먼트는 낭비인듯

--- app/src/main/java/mashup/loling/room/CreateRoomPagerAdapter.kt
+++ app/src/main/java/mashup/loling/room/CreateRoomPagerAdapter.kt
@@ -1,24 +1,73 @@
 package mashup.loling.room
 
-import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentManager
-import android.support.v4.app.FragmentPagerAdapter
+import android.content.Context
+import android.support.v4.view.PagerAdapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_create_room_calendar.view.*
+import kotlinx.android.synthetic.main.fragment_create_room_main.view.*
+import kotlinx.android.synthetic.main.fragment_create_room_new_or_enter.view.*
+import mashup.loling.R
+import java.util.*
 
-class CreateRoomPagerAdapter(fm: FragmentManager?, createRoomMethods : CreateRoomActivity.Companion.ICreateRoomMethods) : FragmentPagerAdapter(fm) {
+class CreateRoomPagerAdapter(val context: Context,
+                             val createRoomMethods : CreateRoomActivity.Companion.ICreateRoomMethods)
+    : PagerAdapter() {
 
-    private val item0CalendarFragment = CreateRoomCalendarFragment.newInstance(createRoomMethods)
-    private val item1MainFragment = CreateRoomMainFragment.newInstance(createRoomMethods)
-    private val item2NewOrEnterFragment = CreateRoomNewOrEnterFragment.newInstance(createRoomMethods)
-    private val item3ExistedLolingListFragment = CreateRoomExistedLolingListFragment.newInstance(createRoomMethods)
+    companion object {
+        val PAGE_CALENDAR = 0
+        val PAGE_MAIN = 1
+        val PAGE_EXISTED_CHK = 2
+        val PAGE_EXISTED_LOLING_LIST = 3
+    }
 
-    override fun getItem(p0: Int): Fragment {
-        return when(p0) {
-            0 -> item0CalendarFragment
-            1 -> item1MainFragment
-            2 -> item2NewOrEnterFragment
-            3 -> item3ExistedLolingListFragment
-            else -> item1MainFragment   //default
+    override fun isViewFromObject(p0: View, p1: Any): Boolean {
+        return p0 == p1
+    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        lateinit var view: ViewGroup
+        val inflater = LayoutInflater.from(context)
+        when(position) {
+            PAGE_CALENDAR-> {
+                view = inflater.inflate(R.layout.fragment_create_room_calendar, container, false) as ViewGroup
+                view.btnOkay.setOnClickListener {
+                    val selDate = Date()
+                    selDate.year = view.calSelectDate.year - 1900
+                    selDate.month = view.calSelectDate.month
+                    selDate.date = view.calSelectDate.dayOfMonth
+                    createRoomMethods.onDateSelectedFromCal(selDate)
+                }
+                view.btnCancel.setOnClickListener { createRoomMethods.onCancelFromCal() }
+            }
+
+            PAGE_EXISTED_CHK-> {
+                view = inflater.inflate(R.layout.fragment_create_room_new_or_enter, container, false) as ViewGroup
+                view.btnCreateNewLoling.setOnClickListener { createRoomMethods.onCreateNewLolingClicked() }
+                view.btnJoinExistedLoling.setOnClickListener { createRoomMethods.onJoinExitedLolingClicked() }
+            }
+
+            PAGE_EXISTED_LOLING_LIST-> {
+                view = inflater.inflate(R.layout.fragment_create_room_existed_loling_list, container, false) as ViewGroup
+
+            }
+            else-> { //PAGE_MAIN
+                view = inflater.inflate(R.layout.fragment_create_room_main, container, false) as ViewGroup
+                view.tvRoomDate.setOnClickListener { createRoomMethods.onSelectRoomTextClicked() }
+                view.btnCreateLoling.setOnClickListener { createRoomMethods.onCreateLolingButtonClicked() }
+            }
         }
+
+        container.addView(view)
+        container.setTag(container.hashCode() + position, view)
+        return view
+    }
+
+
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as View)
     }
 
     override fun getCount(): Int {
