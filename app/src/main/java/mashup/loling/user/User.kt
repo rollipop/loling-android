673d5547da6c53af18a2db5b858f2e87f1849b0f
김유정 <pointu341@gmail.com>
2019-02-17 06:59:07 +0900

    Add ReceivedPaperListActivity
    - Add `receiviedPaperListActivity`, `GridRecyclerView`
    
    Fixes https://github.com/rollipop/loling-android/issues/33

--- app/src/main/java/mashup/loling/user/User.kt
+++ app/src/main/java/mashup/loling/user/User.kt
@@ -6,12 +6,12 @@ import java.util.*
  * 현재 로그인한 유저의 정보를 담고있는 싱글톤
  */
 object User {
-    var id: Int = -1
-    var password: String = ""
+    var id: Int = 123456
+    var password: String = "123456"
     var salt: String = ""
     var encryptedPhoneNumber: String = ""
-    var name: String = ""
-    var phoneNumber: String = ""
+    var name: String = "유댕"
+    var phoneNumber: String = "010-2501-74"
     var birthday: Date = Date()
     var createdAt: Date = Date()
     var anonymous: Boolean = false
