9a76c94280156b5de79d82ea51a24a047942d042
jujin <jujin@jujinkim.com>
2019-02-17 19:33:27 +0900

    롤링페이퍼 그리기 기본 동작

--- app/src/main/java/mashup/loling/drawpaper/Component.kt
+++ app/src/main/java/mashup/loling/drawpaper/Component.kt
@@ -1,89 +1,38 @@
 package mashup.loling.drawpaper
 
-import android.graphics.PointF
 import android.view.MotionEvent
 import android.view.View
+import android.widget.TextView
+import mashup.loling.drawpaper.view.IComponentTouchListener
 
-class Component(val view: View) {
-    var position: PointF = PointF(0f,0f)
-    var rotation: Float = 0f
-    var scale: PointF = PointF(1f, 1f)
-
-    var middlePoint = PointF(0f, 0f)
-    var originTouchPoint = PointF(0f, 0f)
-    var originDistance = 0f
-    var originDegree = 0f
+class Component(val view: View, val componentType: ComponentType, componentSelectedListener: IComponentTouchListener) {
 
     init{
         view.setOnTouchListener { v, event ->
-            onTouchInput(event)
+            if(event.action == MotionEvent.ACTION_DOWN) {
+                componentSelectedListener.onComponentSelected(this)
+            }
             return@setOnTouchListener v.onTouchEvent(event)
         }
-    }
 
-    private fun onTouchInput(ev: MotionEvent) {
-        val lParam = view.layoutParams ?: return
-        var pointerCnt = ev.pointerCount
-        pointerCnt = if(pointerCnt > 2) 2 else pointerCnt   // 최대 멀티터치는 두개까지만 제한
-        originTouchPoint = calculateMiddlePoint(ev)
-
-        when(ev.action and MotionEvent.ACTION_MASK) {
-            MotionEvent.ACTION_POINTER_DOWN -> {    // 터치 다운
-                originTouchPoint = middlePoint
-                if(pointerCnt == 2) {
-                    originDistance = calculateDistance(ev)
-                    originDegree = calculateDegree(ev)
-                }
-            }
-
-            MotionEvent.ACTION_MOVE -> {    // 무-브
-                val dMove = calculateMiddlePoint(ev)
-                dMove.offset(-originTouchPoint.x, -originTouchPoint.y)
-                val dDist = calculateDistance(ev) - originDistance
-                val dDeg = calculateDegree(ev) - originDegree
-
-                // Todo : View's position, rotation, scale
-            }
-
-            MotionEvent.ACTION_POINTER_UP -> {  // 터치 업
-                originTouchPoint = middlePoint
-            }
+        if(view is TextView) {
+            view.setLines(1)
         }
     }
 
-    private fun calculateMiddlePoint(ev: MotionEvent): PointF {
-        val pointerCnt = ev.pointerCount
-        val newPoint = PointF(0f, 0f)
-        for(i in 0 until pointerCnt) {
-            newPoint.offset(ev.getX(i), ev.getY(i))
+    fun rotate(deg: Float) {
+        view.rotation = deg
+    }
+
+    fun scale(scale: Float) {
+        view.scaleX = scale
+        view.scaleY = scale
+
+    }
+
+    companion object {
+        enum class ComponentType {
+            TEXT, IMAGE
         }
-
-        if (pointerCnt > 0) {
-            newPoint.x /= pointerCnt
-            newPoint.y /= pointerCnt
-        }
-
-        return newPoint
-    }
-
-    private fun calculateDistance(ev: MotionEvent): Float {
-        return calculateDistance(
-                ev.getX(0), ev.getY(0),
-                ev.getX(1), ev.getY(1))
-    }
-    private fun calculateDistance(x1: Float, y1: Float, x2: Float, y2: Float): Float {
-        val dx = (x1 - x2).toDouble()
-        val dy = (y1 - y2).toDouble()
-        return Math.sqrt((dx*dx)+(dy*dy)).toFloat()
-    }
-
-    private fun calculateDegree(ev: MotionEvent): Float {
-        return calculateDegree(
-                ev.getX(0), ev.getY(0),
-                ev.getX(1), ev.getY(1))
-    }
-    private fun calculateDegree(x1: Float, y1: Float, x2: Float, y2: Float): Float {
-        val slope = (y1-y2)/(x1-x2)
-        return Math.atan(slope.toDouble()).toFloat()
     }
 }
--- app/src/main/java/mashup/loling/drawpaper/view/DrawPaperActivity.kt
+++ app/src/main/java/mashup/loling/drawpaper/view/DrawPaperActivity.kt
@@ -1,13 +1,144 @@
 package mashup.loling.drawpaper.view
 
+import android.graphics.Color
+import android.graphics.PointF
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.widget.RelativeLayout
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_draw_paper.*
 import mashup.loling.R
+import mashup.loling.drawpaper.Component
 
-class DrawPaperActivity : AppCompatActivity() {
+class DrawPaperActivity : AppCompatActivity(), IComponentTouchListener {
+    override fun onComponentSelected(component: Component) {
+        selectedComponent = component
+    }
+
+    private val componentList: ArrayList<Component> = ArrayList()
+    private var selectedComponent: Component? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_draw_paper)
+
+        btnDrawPaperCamera.setOnClickListener { btnDrawPaperCameraClicked() }
+        btnDrawPaperText.setOnClickListener { btnDrawPaperTextClicked() }
+
     }
+
+    private fun btnDrawPaperCameraClicked() {
+        // Todo : send implicit intent(camera or image)
+    }
+
+    private fun btnDrawPaperTextClicked() {
+        val newComponent = Component(TextView(this), Component.Companion.ComponentType.TEXT, this)
+        (newComponent.view as TextView).text = "TEST TEXT"
+        (newComponent.view as TextView).textSize = 90f
+        (newComponent.view as TextView).setBackgroundColor(Color.BLUE)
+        addComponent(newComponent)
+
+        Log.v(DrawPaperActivity::class.java.simpleName, "New Text Component Added!!")
+    }
+
+    private fun addComponent(newComponent: Component) {
+        componentList.add(newComponent)
+        drawArea.addView(newComponent.view)
+    }
+
+    // variables for position
+    var originTouchPoint = PointF(0f, 0f)
+    var originLeft = 0
+    var originTop = 0
+    // variables for scale
+    var originDistance = 0f
+    var originScale = 0f
+    // variables for rotation
+    var originDegree = 0f
+    var originRotation = 0f
+    override fun onTouchEvent(ev: MotionEvent?): Boolean {
+        if (ev == null) return super.onTouchEvent(ev)
+
+        var pointerCnt = ev.pointerCount
+        pointerCnt = if(pointerCnt > 2) 2 else pointerCnt   // 최대 멀티터치는 두개까지만 제한
+        Log.v(Component::javaClass.name, "Touc View" + pointerCnt)
+
+
+        when(ev.action and MotionEvent.ACTION_MASK) {
+            MotionEvent.ACTION_DOWN -> {    // 1개 터치 다운
+                originTouchPoint.x = ev.getX(0)
+                originTouchPoint.y = ev.getY(0)
+                originDegree = 0f
+                originRotation = 0f
+                selectedComponent?.let {
+                    val lParam: RelativeLayout.LayoutParams = it.view.layoutParams as RelativeLayout.LayoutParams
+                    originLeft = lParam.leftMargin
+                    originTop = lParam.topMargin
+                    originScale = it.view.scaleX
+                    originRotation = it.view.rotation
+                }
+
+            }
+            MotionEvent.ACTION_POINTER_DOWN -> {    // 터치 다운
+                if(pointerCnt == 2) {
+                    originDistance = calculateDistance(ev)
+                    originDegree = calculateDegree(ev)
+                }
+            }
+
+            MotionEvent.ACTION_MOVE -> {    // 무-브
+                selectedComponent?.let {
+                    val lParam: RelativeLayout.LayoutParams = it.view.layoutParams as RelativeLayout.LayoutParams
+                    val dMove = PointF(ev.getX(0), ev.getY(0))
+                    dMove.offset(-originTouchPoint.x, -originTouchPoint.y)
+                    var dDist = calculateDistance(ev) / originDistance
+                    var dDeg = calculateDegree(ev) - originDegree
+
+                    // move position
+                    lParam.leftMargin = originLeft + dMove.x.toInt()
+                    lParam.topMargin = originTop + dMove.y.toInt()
+                    it.view.layoutParams = lParam
+
+                    Log.v("JUJIN", "dist $originScale $dDist, deg $originRotation $dDeg")
+
+                    // rotate/scale using matrix
+                    if(dDist.isNaN() || dDist == 0f) dDist = 1f
+                    it.scale(originScale * dDist)
+                    it.rotate(originRotation + dDeg)
+                }
+            }
+
+            MotionEvent.ACTION_POINTER_UP -> {  // 터치 업
+                selectedComponent = null
+            }
+        }
+
+        return super.onTouchEvent(ev)
+    }
+
+    private fun calculateDistance(ev: MotionEvent): Float {
+        if(ev.pointerCount < 2) return 0f
+        return calculateDistance(
+                ev.getX(0), ev.getY(0),
+                ev.getX(1), ev.getY(1))
+    }
+    private fun calculateDistance(x1: Float, y1: Float, x2: Float, y2: Float): Float {
+        val dx = (x1 - x2).toDouble()
+        val dy = (y1 - y2).toDouble()
+        return Math.sqrt((dx*dx)+(dy*dy)).toFloat()
+    }
+
+    private fun calculateDegree(ev: MotionEvent): Float {
+        if(ev.pointerCount < 2) return 0f
+        return calculateDegree(
+                ev.getX(0), ev.getY(0),
+                ev.getX(1), ev.getY(1))
+    }
+    private fun calculateDegree(x1: Float, y1: Float, x2: Float, y2: Float): Float {
+        val rad = Math.atan2(y1-y2.toDouble(), x1-x2.toDouble())
+        return Math.toDegrees(rad).toFloat()
+    }
+
 }
